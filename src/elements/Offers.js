37e05b8b0d5e7f26216272719c9091a3deac11ef
Artur Świerc <aswierc@gmail.com>
2021-05-09 12:02:56 +0200

    add data grid

--- src/elements/Offers.js
+++ src/elements/Offers.js
@@ -1,69 +1,28 @@
-import {makeStyles} from "@material-ui/core/styles";
 import React from "react";
 import Title from "../layout/Title";
-import Table from "@material-ui/core/Table";
-import TableHead from "@material-ui/core/TableHead";
-import TableRow from "@material-ui/core/TableRow";
-import TableCell from "@material-ui/core/TableCell";
-import TableBody from "@material-ui/core/TableBody";
-import Link from "@material-ui/core/Link";
+import { DataGrid } from '@material-ui/data-grid';
 import useOffers from "../hooks/offers";
 
-const useStyles = makeStyles((theme) => ({
-    seeMore: {
-        marginTop: theme.spacing(3),
-    },
-}));
-
-function preventDefault(event) {
-    event.preventDefault()
-}
+const columns = [
+    // { field: 'offerId', headerName: 'ID', width: 150 },
+    { field: 'name', headerName: 'Produkt', width: 450, sortable: false },
+    { field: 'price', headerName: 'Cena' },
+    { field: 'stock', headerName: 'Ilość' },
+    { field: 'watchersCount', headerName: 'Obserwacje' },
+    { field: 'visitsCount', headerName: 'Wizyt' },
+    { field: 'soldCount', headerName: 'Sprzedano' },
+    { field: 'publicationDate', headerName: 'Data publikacji', width: 150 }
+]
 
 export default function Offers() {
-    const classes = useStyles()
     const offers = useOffers()
-
-    console.log(offers)
 
     return (
         <React.Fragment>
             <Title>Moje oferty</Title>
-            <Table size="small">
-                <TableHead>
-                    <TableRow>
-                        <TableCell>ID</TableCell>
-                        <TableCell>Nazwa</TableCell>
-                        <TableCell>Cena</TableCell>
-                        <TableCell>Ilość</TableCell>
-                        <TableCell>Wizyty</TableCell>
-                        <TableCell>Obserwacje</TableCell>
-                        <TableCell>Sprzedane</TableCell>
-                        {/*<TableCell>Payment Method</TableCell>*/}
-                        {/*<TableCell align="right">Sale Amount</TableCell>*/}
-                    </TableRow>
-                </TableHead>
-                {offers &&
-                    <TableBody>
-                        {offers.map((offer) => (
-                            <TableRow key={offer.offerId}>
-                                <TableCell>{offer.offerId}</TableCell>
-                                <TableCell>{offer.name}</TableCell>
-                                <TableCell>{offer.price}</TableCell>
-                                <TableCell>{offer.stock}</TableCell>
-                                <TableCell>{offer.visitsCount}</TableCell>
-                                <TableCell>{offer.watchersCount}</TableCell>
-                                <TableCell>{offer.soldCount}</TableCell>
-                                {/*<TableCell align="right"></TableCell>*/}
-                            </TableRow>
-                        ))}
-                    </TableBody>
-                }
-            </Table>
-            <div className={classes.seeMore}>
-                <Link color="primary" href="#" onClick={preventDefault}>
-                    Zobacz więcej
-                </Link>
-            </div>
+            {offers &&
+                <DataGrid rows={offers} columns={columns} autoHeight autoPageSize getRowId={(row) => row.offerId}/>
+            }
         </React.Fragment>
     )
 }
